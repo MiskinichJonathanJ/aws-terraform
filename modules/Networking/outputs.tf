fd389a85b005aaaf781ca8e9c48473ff30e10d88
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-08-29 23:41:59 -0300

    agregar outpust de vpc y subnets ids

--- modules/Networking/outputs.tf
+++ modules/Networking/outputs.tf
@@ -1,12 +1,12 @@
 output "vpc_net_id" {
-  value = module.vpc.vpc_id
+  value       = module.vpc.vpc_id
   description = "ID de la vpc de la red de la nube"
 }
 output "subnets_privates_ids" {
-  value = module.vpc.private_subnets
+  value       = module.vpc.private_subnets
   description = "Lista de IDs  de las subnets privadas"
 }
 output "subnets_public_ids" {
-  value = module.vpc.private_subnets
+  value       = module.vpc.private_subnets
   description = "Lista de IDs  de las subnets publicas"
 }
