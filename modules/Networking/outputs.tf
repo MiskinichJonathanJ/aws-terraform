d1aef6fba5da8766b8fcd94d60643747a1e0548d
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-08-29 23:21:41 -0300

    agregar  modulo  independiente  de networking

--- modules/Networking/outputs.tf
+++ modules/Networking/outputs.tf
@@ -1,4 +1,12 @@
-output "instance_public_ip" {
-  value       = ""                                          # The actual value to be outputted
-  description = "The public IP address of the EC2 instance" # Description of what this output represents
+output "vpc_net_id" {
+  value = module.vpc.vpc_id
+  description = "ID de la vpc de la red de la nube"
+}
+output "subnets_privates_ids" {
+  value = module.vpc.private_subnets
+  description = "Lista de IDs  de las subnets privadas"
+}
+output "subnets_public_ids" {
+  value = module.vpc.private_subnets
+  description = "Lista de IDs  de las subnets publicas"
 }
